56126e5914c81a6889dc4db798e2bfb109a38bd8
yiksoon2002 <chongys-wm20@student.tarc.edu.my>
2021-10-28 21:25:44 +0800

    init

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use App\Models\Rgform;
+
+class FormController extends Controller
+{
+    // to add record
+    function addPerson(Request $request)
+    {
+        $person = new Rgform;
+        $person->name = $request->name;
+        $person->email = $request->email;
+        $person->password = bcrypt($request->password);
+        $person->save();
+
+        echo "<script>alert('Successful to register!');</script>";
+
+        return view('form');
+    }
+
+    // to retrieve record
+    function viewPerson()
+    {
+        $tableView = Rgform::all();
+
+        $sql = DB::select('SELECT * FROM rgforms WHERE email =\'ys@gmail.com\'');
+        $date_check = DB::table('rgforms')
+            ->whereDate('created_at', '>', "2021-10-01")
+            ->get();
+
+        return view("personView", ['tableView' => $date_check]);
+    }
+
+    function loginUser(Request $request)
+    {
+        // loop Rgform's data (email & password)
+        foreach (Rgform::get(['email', 'password']) as $i) {
+
+            if ($request->email == $i->email) {
+                if (Hash::check(($request->password), $i->password)) {
+                    echo '<script>alert(\'Login Successfully!\');</script>';
+                    return view("welcome");
+                }
+            }
+        }
+        echo '<script>alert(\'Failed to Login. Check your email or password.\');</script>';
+        return view("login");
+    }
+
+    public static function update(Request $request)
+    {
+
+        $sql = "UPDATE rgforms SET " . $request->option . " = '$request->value' WHERE id = $request->id";
+
+        DB::update($sql);
+    }
+
+    public static function remove(Request $request)
+    {
+
+        DB::table('rgforms')->where('id', $request->id)
+            ->delete();
+    }
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up Form</title>
+    <link rel="stylesheet" href="/form.css">
+</head>
+
+<body>    
+    <form action="" method="POST">
+    @csrf
+        <h1>Sign Up Form</h1>
+        <div class="icon">
+            <i class="fas fa-user-circle"></i>
+        </div>
+        <div class="formcontainer">
+            <div class="container">
+                <label for="uname"><strong>Name</strong></label>
+                <input type="text" placeholder="Enter Username" name="name" required>
+                <label for="mail"><strong>E-mail</strong></label>
+                <input type="text" placeholder="Enter E-mail" name="email" required>
+                <label for="psw"><strong>Password</strong></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
+
+            </div>
+            <button type="submit"><strong>SIGN UP</strong></button>
+
+        </div>
+    </form
+</body>
+
+</html>
